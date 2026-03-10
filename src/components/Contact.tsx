4f0e9c1e9e5bc5c75a70700efde7b18c639b40fc
mitul <rathodmitul002@gmail.com>
2026-03-10 16:01:54 +0530

    first commit

---
+++ src/components/Contact.tsx
@@ -0,0 +1,62 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:rajeshchittyal21@gmail.com" data-cursor="disable">
+                rathodmitul002@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>MCA/BCA</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/mitul-rathod02"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mitul-rathod002"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="#"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Mitul Rathod</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
